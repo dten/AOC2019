7d05664ba683b18cd3aa3beb6ef75a28e8d95712
David Hewson <dev@daveid.co.uk>
2019-12-03 22:32:15 +0000

    make day 2 runnable

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,28 @@ pub mod day1;
 pub mod day2;
 pub mod day3;
 
-fn main() {
-    println!("helloww")
+use std::io::{self, Read};
+
+fn main() -> io::Result<()> {
+    let mut stdin = String::new();
+    io::stdin().read_to_string(&mut stdin)?;
+    let mut input: Vec<usize> = stdin
+    	.trim()
+        .split(',')
+        .map(str::parse::<usize>)
+        .collect::<Result<_, _>>().unwrap();
+    input[1] = 12;
+    input[2] = 2;
+    for x in 0..99 {
+        for y in 0..99 {
+        	let mut input = input.clone();
+        	input[1] = x;
+        	input[2] = y;
+            if day2::day2a(input)[0] == 19690720 {
+    			println!("{}", 100 * x + y);
+    			return Ok(())
+            }
+        }
+    }
+    panic!("nope");
 }
